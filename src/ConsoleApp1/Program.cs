b0fd5b5eb4d1d651a290f7c4f57c71f68d9fa14a
slava <kuncovs19@gmail.com>
2025-03-11 23:17:52 +0300

    feat: add console app for test api connector library

---
+++ src/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using ApiConnector.ApiConnectrors;
+using ApiConnector.Interfaces.Rest;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ConsoleApp1;
+
+public class Program
+{
+	public static async Task Main(string[] args)
+	{
+		var host = CreateHostBuilder(args).Build();
+
+		var restApiConnector = host.Services.GetRequiredService<IRestApiConnector>();
+
+		await restApiConnector.GetNewTradesAsync("BTCUSD", 5);
+	}
+
+	private static IHostBuilder CreateHostBuilder(string[] args)
+	{
+		return Host.CreateDefaultBuilder(args)
+			.ConfigureServices((context, services) =>
+			{
+				services.AddHttpClient<IRestApiConnector, RestApiConnector>((serviceProvider, client) =>
+				{
+					client.BaseAddress = new Uri("https://api-pub.bitfinex.com");
+				});
+			});
+	}
+}
